600a20b6dbaf8cfb46c1bcf1b9d6e9e007391999
Christoph Schranz <christoph.schranz@salzburgresearch.at>
2017-10-03 15:09:28 +0200

    fetching PrinterData and SensorData

--- db-adapter.py
+++ db-adapter.py
@@ -5,8 +5,10 @@ import json
 import logging
 
 from logstash import TCPLogstashHandler
-from kafka import KafkaConsumer
-import concurrent.futures
+# from kafka import KafkaConsumer
+# sudo apt-get install librdkafka-dev python-dev
+# pip3 install confluent-kafka
+from confluent_kafka import Consumer, KafkaError
 
 __date__ = "20 Juli 2017"
 __email__ = "christoph.schranz@salzburgresearch.at"
@@ -14,34 +16,39 @@ __status__ = "Development"
 # 2 Sending multiple topics
 
 KAFKA_TOPICS = ["PrinterData", "SensorData"]
-HOST = 'il063'
+bootstrap_servers = 'il061,il062,il063'
+group_id = "db-adapter"
+host = 'il063'
 
-
-def distribute_tasks():
+def build_pipe():
     logging.basicConfig(level='WARNING')
 
-    pool = concurrent.futures.ThreadPoolExecutor(max_workers=10)
-    pool.map(send_to_logstash, KAFKA_TOPICS)
-
-def send_to_logstash(topic):
     # setup logging
-    logger = logging.getLogger(topic)
+    logger = logging.getLogger(str(KAFKA_TOPICS))
     logger.setLevel(logging.INFO)
-    logstash_handler = TCPLogstashHandler(host=os.getenv('LOGSTASH_HOST', HOST),
+    logstash_handler = TCPLogstashHandler(host=os.getenv('LOGSTASH_HOST', host),
                                           port=int(os.getenv('LOGSTASH_PORT', 5000)),
                                           version=1)
     logger.addHandler(logstash_handler)
 
-    consumer = KafkaConsumer(
-            bootstrap_servers=['il061', 'il062', 'il063'],
-            value_deserializer=lambda m: json.loads(m.decode('utf-8')),
-            api_version=(0, 9))
-    consumer.subscribe([topic])
-    logger.info('Checking topics %s', consumer.subscription())
+    consumer = Consumer({'bootstrap.servers': bootstrap_servers, 'group.id': 'group_id',
+                         'default.topic.config': {'auto.offset.reset': 'smallest'}})
+    consumer.subscribe(KAFKA_TOPICS)
+    logger.info('Checking topics: {}'.format(str(KAFKA_TOPICS)))
 
-    for msg in consumer:
-        logger.info(topic, extra=msg.value)
+    running = True
+    while running:
+        msg = consumer.poll()
+        if not msg.error():
+            data = json.loads(msg.value().decode('utf-8'))
+            print(data)
+            logger.info('', extra=data)
+        elif msg.error().code() != KafkaError._PARTITION_EOF:
+            print(msg.error())
+            running = False
+
+    consumer.close()
 
 
 if __name__ == '__main__':
-    distribute_tasks()
+    build_pipe()
